1676cd19d3d6bfbfbc142f83c667bfdb7f4781e8
Alan Mejia <alan-mejia@outlook.com>
2019-03-19 08:09:35 -0600

    add work section

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 
-$(function(){
-    $(document).on("scroll", function(){
-        let $navbar= $(".navigation");
-        $navbar.toggleClass("scrolled", $(this).scrollTop()>$navbar.height());
+$(function () {
+    $(document).on("scroll", function () {
+        let $navbar = $(".navigation");
+        $navbar.toggleClass("scrolled", $(this).scrollTop() > $navbar.height());
     });
 
-    $(".navigation li:first-child").on("click", function(){
+    $(".navigation li").on("click", function (event) {
+        console.log(event.target);
         $("html, body").animate({
             scrollTop: $(".section-education").offset().top
-         }, 2000);
+        }, 2000);
     });
 });
